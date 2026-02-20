57945dc4ba004750386dbd3a688336d850dab377
Linus Jokela <linusjokela@gmail.com>
2026-02-20 11:26:54 +0100

    Fix: Fixed env variable in index.ts for db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ import * as authSchema from "./auth-schema";
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  database: process.env.DB_NAME || 'battleships',
+  database: process.env.DB_DATABASE || 'battleships',
   port: Number(process.env.DB_PORT) || 3306,
   password: process.env.DB_PASSWORD || ''
 });
