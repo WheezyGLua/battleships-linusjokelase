30deff65d0bcaede7baecbdb158c0ffb8ef249e4
Linus Jokela <linusjokela@gmail.com>
2026-02-20 13:31:51 +0100

    Fix DB env var names and add fallbacks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,11 @@ import { createPool } from "mysql2/promise"
 
 export const auth = betterAuth({
     database: createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        port: Number(process.env.DB_PORT),
-        password: process.env.DB_PASSWORD
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        database: process.env.DB_DATABASE || 'battleships',
+        port: Number(process.env.DB_PORT) || 3306,
+        password: process.env.DB_PASSWORD || ''
     }),
     emailAndPassword: {
         enabled: true
