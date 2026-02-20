692092f37d8fe10305e9ed190862a9962268608c
Linus Jokela <linusjokela@gmail.com>
2026-02-20 11:23:37 +0100

    Fix: Fixed env variable in cgeck-tables

--- src/scripts/check-tables.ts
+++ src/scripts/check-tables.ts
@@ -8,7 +8,7 @@ async function check() {
   const pool = createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    database: process.env.DB_NAME || 'battleships',
+    database: process.env.DB_DATABASE || 'battleships',
     port: Number(process.env.DB_PORT) || 3306,
     password: process.env.DB_PASSWORD || ''
   });
